7ca33503104c119f4a56d56ae32f91addacf16d8
Ars <asd>
2023-09-13 22:12:06 +0300

    init commit
    (cherry picked from commit e5ab32c96a558b397e1be433bd2b9de65636798f)

---
+++ src/main/java/com/example/testovoezadanierestservicestringcounter/TestovoeZadanieRestServiceStringCounterApplication.java
@@ -0,0 +1,13 @@
+package com.example.testovoezadanierestservicestringcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestovoeZadanieRestServiceStringCounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestovoeZadanieRestServiceStringCounterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testovoezadanierestservicestringcounter/TestovoeZadanieRestServiceStringCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testovoezadanierestservicestringcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestovoeZadanieRestServiceStringCounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
